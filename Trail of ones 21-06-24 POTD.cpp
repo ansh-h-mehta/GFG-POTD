6fe72c5bd8c38ee476d8bbff70da838714d368e2
Ansh H Mehta <anshhmehta49@gmail.com>
2024-06-21 23:30:07 +0530

    Create Trail of ones 21-06-24 POTD.cpp

---
+++ Trail of ones 21-06-24 POTD.cpp
@@ -0,0 +1,16 @@
+// https://www.geeksforgeeks.org/problems/trail-of-ones3242/1
+
+class Solution {
+  public:
+    int numberOfConsecutiveOnes(int n) {
+        if(n==2)return 1;
+        int a=0,b=1,ans=1,mod=1e9+7;
+        for(int i=3; i<=n; i++){
+            int c=(a+b)%mod;
+            a=b;
+            b=c;
+            ans=((ans*2)%mod+c)%mod;
+        }
+        return ans;
+    }
+};
