db629dfe109db52613dd53921f04e42c8a2942ea
Ansh H Mehta <anshhmehta49@gmail.com>
2024-06-27 11:51:23 +0530

    Create Toeplitz matrix 27-06-24 POTD.cpp

---
+++ Toeplitz matrix 27-06-24 POTD.cpp
@@ -0,0 +1,26 @@
+// https://www.geeksforgeeks.org/problems/toeplitz-matrix/1
+
+bool solve(int i,int j,vector<vector<int>>&mat){
+    bool flag=true;    
+    while(i<mat.size()-1 && j<mat[0].size()-1){
+        if(mat[i][j]==mat[i+1][j+1])
+        flag=true;
+        else
+        return false;
+        i=i+1;
+        j=j+1;
+    }  
+    return flag;
+}
+
+bool isToepliz(vector<vector<int>>& mat) {
+    bool flag=true;
+    for(int i=0;i<mat.size();i++){
+        for(int j=0;j<mat[0].size();j++){
+            flag=solve(i,j,mat);
+            if(flag==false)
+            return false;       
+        }
+    }   
+    return flag;
+}
