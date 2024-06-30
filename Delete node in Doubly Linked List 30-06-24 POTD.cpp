12b9080ab4150cac0b353f1cef516d04961a78c3
Ansh H Mehta <anshhmehta49@gmail.com>
2024-06-30 19:48:26 +0530

    Create Delete node in Doubly Linked List 30-06-24 POTD.cpp

---
+++ Delete node in Doubly Linked List 30-06-24 POTD.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    Node* deleteNode(Node* head, int x) {
+        
+        struct Node *del = head;
+        x = x-1;
+        while(x--){
+            del = del->next;
+        }
+        
+        if(head == NULL || del == NULL){
+            return NULL;
+        }
+        if(head == del){
+            head = del->next;
+        }
+        if(del->next != NULL){
+            del->next->prev = del->prev;
+        }
+        if(del->prev !=NULL){
+            del->prev->next = del->next;
+        }
+        free(del);
+        return head;
+    }
+};
