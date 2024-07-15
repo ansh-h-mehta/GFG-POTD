92aaaa60bea910587af9bbfea012cdc4a3f3f560
Ansh H Mehta <anshhmehta49@gmail.com>
2024-07-15 22:43:41 +0530

    Create Smallest number 15-07-24 POTD.cpp

---
+++ Smallest number 15-07-24 POTD.cpp
@@ -0,0 +1,18 @@
+// https://www.geeksforgeeks.org/problems/smallest-number5829/1
+
+class Solution {
+  public:
+    string smallestNumber(int s, int d) {
+        if(s>9*d) return "-1";
+        string res(d,' ');
+        s--;
+        for(int i=d-1;i>=1;i--){
+            int diff=min(9,s); 
+            res[i]='0'+diff;
+            s-=diff;
+        }
+        
+        res[0]='1'+s;
+        return res;
+    }
+};
