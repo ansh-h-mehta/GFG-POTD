a66c92933bda07d2fb9c5ee8880e0f2ec952995e
Knightmare Gaming <anshhmehta49@gmail.com>
2024-08-08 23:51:34 +0530

    Create Sum tree 08-08-24 POTD.cpp

---
+++ Sum tree 08-08-24 POTD.cpp
@@ -0,0 +1,25 @@
+// https://www.geeksforgeeks.org/problems/sum-tree/1
+
+class Solution
+{
+private:
+    bool res = true;
+
+public:
+    int f(Node *n)
+    {
+        if (!n)
+            return 0;
+        int ls = f(n->left), rs = f(n->right);
+        if (!n->left && !n->right)
+            return n->data;
+        if (n->data != ls + rs)
+            res = false;
+        return n->data + ls + rs;
+    }
+    bool isSumTree(Node *root)
+    {
+        f(root);
+        return res;
+    }
+};
