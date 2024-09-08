d165f8f4d30432193bccbb80e62616c05261c479
Knightmare Gaming <anshhmehta49@gmail.com>
2024-09-08 23:32:48 +0530

    Create Minimum Jumps 08-09-24 POTD.cpp

---
+++ Minimum Jumps 08-09-24 POTD.cpp
@@ -0,0 +1,30 @@
+// www.geeksforgeeks.org/problems/minimum-number-of-jumps-1587115620/1
+
+class Solution
+{
+public:
+    int minJumps(vector<int> &arr)
+    {
+        // Your code here
+        int jump = 0;
+        int n = arr.size();
+        int maxi = 0;
+        int mrange = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            maxi = max(maxi, arr[i] + i);
+            if (mrange == i)
+            {
+                mrange = maxi;
+                jump++;
+                if (mrange >= n - 1)
+                {
+                    return jump;
+                }
+            }
+        }
+
+        return -1;
+    }
+};
