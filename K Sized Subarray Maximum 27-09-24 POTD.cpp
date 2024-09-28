06c37bde2e6435ccdef18314e169fe6de758dac3
Knightmare Gaming <anshhmehta49@gmail.com>
2024-09-28 12:35:11 +0530

    Create K Sized Subarray Maximum 27-09-24 POTD.cpp

---
+++ K Sized Subarray Maximum 27-09-24 POTD.cpp
@@ -0,0 +1,32 @@
+// https://www.geeksforgeeks.org/problems/maximum-of-all-subarrays-of-size-k3101/1
+
+class Solution
+{
+public:
+    vector<int> max_of_subarrays(int k, vector<int> &arr)
+    {
+        int n = arr.size();
+        int i = 0, j = 1, maxi = 0;
+        vector<int> ans;
+
+        while (i < k)
+        {
+            if (arr[i] > maxi)
+                maxi = arr[i];
+            i++;
+        }
+        ans.push_back(maxi);
+
+        while (i < n)
+        {
+            if (arr[j - 1] == maxi)
+                maxi = *max_element(arr.begin() + j, arr.begin() + i + 1);
+            else if (arr[i] > maxi)
+                maxi = arr[i];
+            ans.push_back(maxi);
+            i++;
+            j++;
+        }
+        return ans;
+    }
+};
